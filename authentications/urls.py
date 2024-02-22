f7fc62f5ce9c44ad4069e7ff392400c24964f65b
Matheus Kepy <matheuskdev@gmail.com>
2024-02-22 08:08:08 -0300

    Update authentications app: adjusted for flake8 compliance.

--- authentications/urls.py
+++ authentications/urls.py
@@ -3,5 +3,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path('authentications/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'authentications/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
 ]
