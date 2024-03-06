c193a476dc72873cf3ded00e20618ba08b9779cf
Matheus Kepy <matheuskdev@gmail.com>
2024-03-06 15:44:28 -0300

    add user serializer in response of ideas

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+
+from accounts.serializers import UserSerializer
 from .models import Like
 
 class LikePinSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Like
-        fields = ['pin', 'created_at']
+        fields = ['user','pin', 'created_at']
